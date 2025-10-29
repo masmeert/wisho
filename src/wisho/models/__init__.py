f95f1bcd4fdb5db156f59d7df7d98456b14c882c
Massimo <massimo@adimian.com>
2025-10-29 17:11:50 +0100

    fix pydantic to sqla issues

---
+++ src/wisho/models/__init__.py
@@ -0,0 +1,19 @@
+from wisho.models.entry import Entry
+from wisho.models.entry_priority import EntryPriority
+from wisho.models.gloss import Gloss
+from wisho.models.kanji import Kanji
+from wisho.models.reading import Reading
+from wisho.models.reading_restriction import ReadingRestriction
+from wisho.models.sense import Sense
+from wisho.models.sense_pos import SensePOS
+
+__all__ = [
+    "Entry",
+    "EntryPriority",
+    "Gloss",
+    "Kanji",
+    "Reading",
+    "ReadingRestriction",
+    "Sense",
+    "SensePOS",
+]
